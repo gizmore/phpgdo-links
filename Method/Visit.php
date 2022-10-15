1de578fc0b5acd885ff219280dfeec0416da5098
gizmore <gizmore@wechall.net>
2022-10-15 11:38:17 +0200

    '7.0.1-r1664 gg'

--- Method/Visit.php
+++ Method/Visit.php
@@ -1,12 +1,15 @@
 <?php
 namespace GDO\Links\Method;
+
 use GDO\Core\Method;
-use GDO\Core\Website;
 use GDO\Links\GDO_Link;
 use GDO\User\GDO_User;
 use GDO\Util\Common;
+use GDO\UI\GDT_Redirect;
+
 /**
  * Increase visit counter and redirect.
+ * 
  * @author gizmore
  */
 final class Visit extends Method
@@ -18,7 +21,7 @@ final class Visit extends Method
 		$level = $link->getLevel();
 		if ($level > $user->getLevel())
 		{
-			return $this->error('err_link_level', [$level])->addField(GDT_Redirect::toBack(5));
+			return $this->error('err_link_level', [$level])->addField(GDT_Redirect::hrefBack());
 		}
 		
 		$link->increase('link_views');
